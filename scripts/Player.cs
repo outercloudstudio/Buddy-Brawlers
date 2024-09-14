f973b2a7634a5cc559d4cd2ffda39d707241a480
Outer Cloud Studio <outercloudstudio@gmail.com>
2024-09-14 11:08:42 -0500

    Polishing animations

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -17,17 +17,21 @@ public partial class Player : CharacterBody3D, NetworkPointUser
 
 	private float _yVelocity;
 	private Node3D _model;
+	private AnimationTree _visualAnimationTree;
 	private AnimationTree _animationTree;
-	private AnimationPlayer _moveAnimationPlayer;
+	private AnimationPlayer _animationPlayer;
 	private Node3D _flipOrigin;
 	private Area3D _attackArea;
 	private Vector3 _knockback;
+	private float _movement;
+	private bool _lastMovedRight = true;
 
 	public override void _Ready()
 	{
 		_model = GetNode<Node3D>("Model");
+		_visualAnimationTree = GetNode<AnimationTree>("VisualAnimationTree");
 		_animationTree = GetNode<AnimationTree>("AnimationTree");
-		_moveAnimationPlayer = GetNode<AnimationPlayer>("MoveAnimationPlayer");
+		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
 		_flipOrigin = GetNode<Node3D>("FlipOrigin");
 		_attackArea = _flipOrigin.GetNode<Area3D>("AttackArea");
 
@@ -50,6 +54,11 @@ public partial class Player : CharacterBody3D, NetworkPointUser
 		if (@event.IsActionPressed("special")) SpecialAttack();
 	}
 
+	public override void _Process(double delta)
+	{
+		UpdateAnimationTree((float)delta);
+	}
+
 	public override void _PhysicsProcess(double delta)
 	{
 		_yVelocity -= Gravity * (float)delta;
@@ -64,35 +73,31 @@ public partial class Player : CharacterBody3D, NetworkPointUser
 		_networkedVelocity.Sync();
 		_networkedMovement.Sync();
 
-		float movement = Input.GetAxis("right", "left");
+		_movement = Input.GetAxis("right", "left");
 
 		if (NetworkPoint.IsOwner)
 		{
-			Velocity = Vector3.Forward * movement * Speed + Vector3.Up * _yVelocity + _knockback;
+			Velocity = Vector3.Forward * _movement * Speed + Vector3.Up * _yVelocity + _knockback;
 
 			MoveAndSlide();
-			UpdateAnimationTree(movement);
 
 			_networkedPosition.Value = GlobalPosition;
 			_networkedVelocity.Value = Velocity;
-			_networkedMovement.Value = movement;
+			_networkedMovement.Value = _movement;
 		}
 		else
 		{
 			GlobalPosition = GlobalPosition.Lerp(_networkedPosition.Value, (float)delta * 20.0f);
 			Velocity = _networkedVelocity.Value;
 
-			movement = _networkedMovement.Value;
+			_movement = _networkedMovement.Value;
 
 			MoveAndSlide();
-			UpdateAnimationTree(_networkedMovement.Value);
 		}
 
-		if (movement != 0)
-		{
-			_model.Scale = new Vector3(1, 1, movement > 0 ? -1 : 1);
-			_flipOrigin.Scale = new Vector3(1, 1, movement > 0 ? -1 : 1);
-		}
+		if (_movement != 0) _lastMovedRight = _movement < 0;
+
+		_flipOrigin.Scale = new Vector3(1, 1, _lastMovedRight ? 1 : -1);
 	}
 
 	public void Damage(Vector3 knockback)
@@ -105,9 +110,10 @@ public partial class Player : CharacterBody3D, NetworkPointUser
 		});
 	}
 
-	private void UpdateAnimationTree(float movement)
+	private void UpdateAnimationTree(float delta)
 	{
-		_animationTree.Set("parameters/WalkBlend/blend_amount", movement != 0 ? 1 : 0);
+		_visualAnimationTree.Set("parameters/WalkBlend/blend_amount", MathHelper.FixedLerp((float)_visualAnimationTree.Get("parameters/WalkBlend/blend_amount"), _movement != 0 ? 1 : 0, 8f, delta));
+		_animationTree.Set("parameters/TurnBlendSpace/blend_position", MathHelper.FixedLerp((float)_animationTree.Get("parameters/TurnBlendSpace/blend_position"), _lastMovedRight ? 1 : -1, 12f, delta));
 	}
 
 	public virtual void TriggerNormalAttack()
@@ -152,28 +158,32 @@ public partial class Player : CharacterBody3D, NetworkPointUser
 
 	private void NormalAttack()
 	{
+		if (_animationPlayer.IsPlaying()) return;
+
 		NetworkPoint.BounceRpcToClientsFast(nameof(NormalAttackRpc));
 	}
 
 	private void SpecialAttack()
 	{
+		if (_animationPlayer.IsPlaying()) return;
+
 		NetworkPoint.BounceRpcToClientsFast(nameof(SpecialAttackRpc));
 	}
 
 	private void NormalAttackRpc(Message message)
 	{
-		_animationTree.Set("parameters/NormalOneShot/request", (int)AnimationNodeOneShot.OneShotRequest.Fire);
+		_visualAnimationTree.Set("parameters/NormalOneShot/request", (int)AnimationNodeOneShot.OneShotRequest.Fire);
 
-		_moveAnimationPlayer.Stop();
-		_moveAnimationPlayer.Play("normal");
+		_animationPlayer.Stop();
+		_animationPlayer.Play("normal");
 	}
 
 	private void SpecialAttackRpc(Message message)
 	{
-		_animationTree.Set("parameters/SpecialOneShot/request", (int)AnimationNodeOneShot.OneShotRequest.Fire);
+		_visualAnimationTree.Set("parameters/SpecialOneShot/request", (int)AnimationNodeOneShot.OneShotRequest.Fire);
 
-		_moveAnimationPlayer.Stop();
-		_moveAnimationPlayer.Play("special");
+		_animationPlayer.Stop();
+		_animationPlayer.Play("special");
 	}
 
 	private void DamageRpc(Message message)
