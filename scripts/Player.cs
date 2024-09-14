449e7c0f7821de3ed763145886c466b36eeb70f9
Outer Cloud Studio <outercloudstudio@gmail.com>
2024-09-14 03:22:11 -0500

    Combat

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -18,11 +18,16 @@ public partial class Player : CharacterBody3D, NetworkPointUser
 	private float _yVelocity;
 	private Node3D _model;
 	private AnimationTree _animationTree;
+	private Node3D _flipOrigin;
+	private Area3D _attackArea;
+	private Vector3 _knockback;
 
 	public override void _Ready()
 	{
 		_model = GetNode<Node3D>("Model");
 		_animationTree = GetNode<AnimationTree>("AnimationTree");
+		_flipOrigin = GetNode<Node3D>("FlipOrigin");
+		_attackArea = _flipOrigin.GetNode<Area3D>("AttackArea");
 
 		NetworkPoint.Setup(this);
 
@@ -32,6 +37,7 @@ public partial class Player : CharacterBody3D, NetworkPointUser
 
 		NetworkPoint.Register(nameof(NormalAttackRpc), NormalAttackRpc);
 		NetworkPoint.Register(nameof(SpecialAttackRpc), SpecialAttackRpc);
+		NetworkPoint.Register(nameof(DamageRpc), DamageRpc);
 	}
 
 	public override void _Input(InputEvent @event)
@@ -46,6 +52,8 @@ public partial class Player : CharacterBody3D, NetworkPointUser
 	{
 		_yVelocity -= Gravity * (float)delta;
 
+		_knockback = MathHelper.FixedLerp(_knockback, Vector3.Zero, 4f, (float)delta);
+
 		if (IsOnFloor() && _yVelocity < 0) _yVelocity = 0;
 
 		if (IsOnFloor() && Input.IsActionPressed("up")) _yVelocity = Jump;
@@ -58,7 +66,7 @@ public partial class Player : CharacterBody3D, NetworkPointUser
 
 		if (NetworkPoint.IsOwner)
 		{
-			Velocity = Vector3.Forward * movement * Speed + Vector3.Up * _yVelocity;
+			Velocity = Vector3.Forward * movement * Speed + Vector3.Up * _yVelocity + _knockback;
 
 			MoveAndSlide();
 			UpdateAnimationTree(movement);
@@ -79,7 +87,20 @@ public partial class Player : CharacterBody3D, NetworkPointUser
 		}
 
 		if (movement != 0)
+		{
 			_model.Scale = new Vector3(1, 1, movement > 0 ? -1 : 1);
+			_flipOrigin.Scale = new Vector3(1, 1, movement > 0 ? -1 : 1);
+		}
+	}
+
+	public void Damage(Vector3 knockback)
+	{
+		NetworkPoint.BounceRpcToClients(nameof(DamageRpc), message =>
+		{
+			message.AddFloat(knockback.X);
+			message.AddFloat(knockback.Y);
+			message.AddFloat(knockback.Z);
+		});
 	}
 
 	private void UpdateAnimationTree(float movement)
@@ -90,11 +111,45 @@ public partial class Player : CharacterBody3D, NetworkPointUser
 	private void NormalAttack()
 	{
 		NetworkPoint.BounceRpcToClientsFast(nameof(NormalAttackRpc));
+
+		var bodies = _attackArea.GetOverlappingBodies();
+
+		foreach (Node body in bodies)
+		{
+			if (body == this) continue;
+
+			if (!(body is Player otherPlayer)) continue;
+
+			Vector3 playerOffset = otherPlayer.GlobalPosition - GlobalPosition;
+			playerOffset.Y = 0;
+			playerOffset.X = 0;
+
+			playerOffset = playerOffset.Normalized();
+
+			otherPlayer.Damage(playerOffset * 7f + Vector3.Up * 5f);
+		}
 	}
 
 	private void SpecialAttack()
 	{
 		NetworkPoint.BounceRpcToClientsFast(nameof(SpecialAttackRpc));
+
+		var bodies = _attackArea.GetOverlappingBodies();
+
+		foreach (Node body in bodies)
+		{
+			if (body == this) continue;
+
+			if (!(body is Player otherPlayer)) continue;
+
+			Vector3 playerOffset = otherPlayer.GlobalPosition - GlobalPosition;
+			playerOffset.Y = 0;
+			playerOffset.X = 0;
+
+			playerOffset = playerOffset.Normalized();
+
+			otherPlayer.Damage(playerOffset * 7f + Vector3.Up * 10f);
+		}
 	}
 
 	private void NormalAttackRpc(Message message)
@@ -105,5 +160,12 @@ public partial class Player : CharacterBody3D, NetworkPointUser
 	private void SpecialAttackRpc(Message message)
 	{
 		_animationTree.Set("parameters/SpecialOneShot/request", (int)AnimationNodeOneShot.OneShotRequest.Fire);
+	}
+
+	private void DamageRpc(Message message)
+	{
+		Vector3 knockback = new Vector3(message.GetFloat(), message.GetFloat(), message.GetFloat());
+
+		_knockback = knockback;
 	}
 }
